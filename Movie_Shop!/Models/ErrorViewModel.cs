35ba68c46ef5ef2c6322577b0c71229624bb606c
Basel <basel.eibo4676@gmail.com>
2024-11-10 16:55:25 +0100

    Add project files.

---
+++ Movie_Shop!/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Movie_Shop_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
