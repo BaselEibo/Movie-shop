d16c31ee8044feb64b779ce0717e0a80dd2fac3d
Basel <basel.eibo4676@gmail.com>
2024-11-22 18:13:21 +0100

    Add validations and data annotations to Movie model

--- Movie_Shop!/Models/Movie.cs
+++ Movie_Shop!/Models/Movie.cs
@@ -5,10 +5,20 @@ namespace Movie_Shop_.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Title { get; set; } = string.Empty;
+
         public string Director { get; set; } = string.Empty;
+
+        [Display(Name = "Release Year")]
         public int ReleaseYear { get; set; }
+
+        [Range(0, (double)decimal.MaxValue, ErrorMessage = "The Price Must be a Positive Value")]
         public decimal Price { get; set; }
+
+        [Url]
         public string ImageUrl { get; set; } = string.Empty;
     }
 }
