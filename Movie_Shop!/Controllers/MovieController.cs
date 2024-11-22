8e5c9fec3a44259837afa7985a362bbb43d435d8
Basel <basel.eibo4676@gmail.com>
2024-11-22 21:11:16 +0100

    Add basic MovieController structure

---
+++ Movie_Shop!/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Shop_.Models;
+using Movie_Shop_.Repositorey;
+
+namespace Movie_Shop_.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly IMovieServices _MS;
+        private readonly ILogger<MovieController> _logger;
+       
+
+        public MovieController(IMovieServices Ms, ILogger<MovieController> logger)
+        {
+            _MS = Ms;
+            _logger = logger;
+          
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
