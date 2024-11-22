a8f9b70351e31ecbfdd78a1d0ddceb552c5315d9
Basel <basel.eibo4676@gmail.com>
2024-11-22 20:56:33 +0100

    Add IMovieServices interface

---
+++ Movie_Shop!/Repositorey/IMovieServices.cs
@@ -0,0 +1,19 @@
+﻿using Movie_Shop_.Models;
+
+namespace Movie_Shop_.Repositorey
+{
+    public interface IMovieServices
+    {
+        public List<Movie> GetAllMovies();
+
+        public bool CreateMovie(Movie movie);
+
+        public bool UpdateMovie(Movie movie);
+
+        public bool DeleteMovie(int id);
+
+        public Movie GetMovieById(int id);
+
+        public bool AddMovieIfNotExists(Movie movie);
+    }
+}
