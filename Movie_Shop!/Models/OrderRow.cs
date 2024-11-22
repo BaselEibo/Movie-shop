b5c28a86e8afba5434f09eda77a0464daa99a112
Basel <basel.eibo4676@gmail.com>
2024-11-22 18:44:17 +0100

    Add OrderRow model with basic properties

---
+++ Movie_Shop!/Models/OrderRow.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movie_Shop_.Models
+{
+    public class OrderRow
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public int OrderId { get; set; }
+        public int MovieId { get; set; }
+        public Movie? movie { get; set; }
+        public Order? order { get; set; }
+    }
+}
