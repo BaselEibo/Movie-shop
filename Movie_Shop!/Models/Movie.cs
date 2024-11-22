1ddfa831ea94881c9c1cc34089b1a60bde3e51c4
Basel <basel.eibo4676@gmail.com>
2024-11-22 18:11:34 +0100

    Add Movie model with basic properties

---
+++ Movie_Shop!/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Shop_.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Director { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
