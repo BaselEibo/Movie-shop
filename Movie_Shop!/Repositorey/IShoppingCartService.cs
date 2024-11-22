90a6e70d5c1f583e32642694c39ee3978a1936e3
Basel <basel.eibo4676@gmail.com>
2024-11-22 21:57:16 +0100

    Add IShoppingCartService interface with shopping cart operations

---
+++ Movie_Shop!/Repositorey/IShoppingCartService.cs
@@ -0,0 +1,15 @@
+﻿using Movie_Shop_.Models.ViewModels;
+
+namespace Movie_Shop_.Repositorey
+{
+    public interface IShoppingCartService
+    {
+        List<ShoppingCartItem> GetCart();
+        void AddToCart(ShoppingCartItem item);
+        void RemoveFromCart(int movieId);
+        void ChangeCartItemQuantity (int movieId, int quantity);
+        void ClearCart();
+        decimal GetCartTotal();
+        void SaveCart(List<ShoppingCartItem> cart);
+    }         
+}
