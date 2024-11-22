5ead2d0e21994da31ac1273c881b17655362ab23
Basel <basel.eibo4676@gmail.com>
2024-11-22 18:41:50 +0100

    Add data annotations and relationships to Order model

--- Movie_Shop!/Models/Order.cs
+++ Movie_Shop!/Models/Order.cs
@@ -7,9 +7,18 @@ namespace Movie_Shop_.Models
     public class Order
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Customer Id is required.")]
+        [ForeignKey("Customer")]
+        [Display(Name = "Customer Id")]
         public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Order Date is required.")]
+        [Display(Name = "Order Date")]
         public DateTime OrderDate { get; set; }
+
         public Customer? Customers { get; set; }
+
         public List<OrderRow> orderRows { get; set; } = new List<OrderRow>();
     }
 }
