b5d873769f1f1e3fad4871c01fd0f84648f2f0bf
Basel <basel.eibo4676@gmail.com>
2024-11-22 20:00:25 +0100

    initilaize MovieController

---
+++ Movie_Shop!/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Shop_.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+
+
+}
