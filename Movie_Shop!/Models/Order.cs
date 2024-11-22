caf2ff53fe9612d3527fbd100e66af866d310f57
Basel <basel.eibo4676@gmail.com>
2024-11-22 18:41:15 +0100

    Add Order model with basic properties

---
+++ Movie_Shop!/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Movie_Shop_.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Customer? Customers { get; set; }
+        public List<OrderRow> orderRows { get; set; } = new List<OrderRow>();
+    }
+}
