e61779102ec05ce487bf0d5bef90ab0ec8d6e1e1
Basel <basel.eibo4676@gmail.com>
2024-11-22 18:37:34 +0100

    Add Customer model with basic properties

---
+++ Movie_Shop!/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie_Shop_.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string BillingAddress { get; set; } = string.Empty;
+        public string BillingZip { get; set; } = string.Empty;
+        public string BillingCity { get; set; } = string.Empty;
+        public string DeliveryAddress { get; set; } = string.Empty;
+        public string DeliverZip { get; set; } = string.Empty;
+        public string DeliverCity { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string PhoneNo { get; set; } = string.Empty;
+        public List<Order> Orders { get; set; } = new List<Order>();
+    }
+}
