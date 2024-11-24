fc8bed8343391249b7f58424b06fa1b6d39466d5
Basel <basel.eibo4676@gmail.com>
2024-11-24 23:08:00 +0100

    Create CheckoutController.

---
+++ Movie_Shop!/Controllers/checkoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie_Shop_.Controllers
+{
+    public class checkoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
