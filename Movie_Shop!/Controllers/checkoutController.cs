5313089e40fa66851732575995838002f38a65e7
Basel <basel.eibo4676@gmail.com>
2024-11-24 23:11:59 +0100

    Add depandicy ICheckoutService and IShoppingCartService .

--- Movie_Shop!/Controllers/checkoutController.cs
+++ Movie_Shop!/Controllers/checkoutController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Shop_.Repositorey;
 
 namespace Movie_Shop_.Controllers
 {
     public class checkoutController : Controller
     {
+        private readonly ICheckoutService _checkoutService;
+        private readonly IShoppingCartService _shoppingCartService;
+        public checkoutController(ICheckoutService checkoutService , IShoppingCartService shoppingCartService)
+        {
+            _checkoutService = checkoutService;
+            _shoppingCartService = shoppingCartService;
+        }
         public IActionResult Index()
         {
             return View();
